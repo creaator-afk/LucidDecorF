ac322b29804a5867795432647e455f1013a3c9c9
Kshitij Satpathy <akuma10901@gmail.com>
2024-10-14 23:47:21 +0530

    DEV:developed basic structure for the site

---
+++ frontend/LucidDecor/src/app/app.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot([]) // Add your routes here
+  ],
+  providers: []
+})
+export class AppModule { }
--- frontend/LucidDecor/src/main.ts
+++ frontend/LucidDecor/src/main.ts
@@ -1,6 +1,10 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { importProvidersFrom } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+bootstrapApplication(AppComponent, {
+  providers: [
+    importProvidersFrom(RouterModule.forRoot([])) // Add your routes here
+  ]
+}).catch(err => console.error(err));
