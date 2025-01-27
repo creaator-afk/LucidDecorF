8713262f983dd128aff3be30c43657725935535d
Kshitij satpathy <71308896+creaator-afk@users.noreply.github.com>
2025-01-27 19:45:00 +0530

    Merge pull request #3
    * DEV: added basic project structure for spring boot
    
    * DEV: added health checks for servers
    
    * DEV: added GitHub Actions workflows for frontend and backend builds a…

---
+++ src/main/java/com/backend/luciddecorf/health/CustomHealthIndicator.java
@@ -0,0 +1,24 @@
+package com.backend.luciddecorf.health;
+
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.HealthIndicator;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CustomHealthIndicator implements HealthIndicator {
+    @Override
+    public Health health() {
+        //Custom logic to check health of a specific service or component
+        boolean isHealthy = checkServiceHealth();
+        if (isHealthy) {
+            return Health.up().build();
+        }else {
+            return Health.up().withDetail("Error", "Lucid Decor service is down").build();
+        }
+    }
+
+    private boolean checkServiceHealth() {
+        // Implement your custom health check logic here
+        return true;
+    }
+}
